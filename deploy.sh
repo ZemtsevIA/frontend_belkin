20e382afcbc5d0009db44d212663b768354f0974
Zemtsev <ZemtsevIA@yandex.ru>
2025-12-20 01:27:47 +0700

    Перенос на новый репозиторий

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<ZemtsevIA>/<frontend_belkin>.git master:gh-pages
+git push -f git@github.com:<LazichevaMA>/<IP_Belkin>.git master:gh-pages
 
 cd -
